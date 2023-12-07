544d4f91f1276fd4ec84ba2f9cc1f0e37274eb48
Aysha Q <aysha.qures@outlook.com>
2023-12-07 18:18:55 -0500

    Update FINALjavascript.js
    updates dec 07

--- FINALjavascript.js
+++ FINALjavascript.js
@@ -1,3 +1,8 @@
+////////////////////
+// Owner : Chloe Nevin
+// Date  : Dec 7
+// From  : https://www.w3schools.com/
+////////////////////
 function myFunction(elementId) {
     var x = document.getElementById(elementId);
     if (x.style.display === "none") {
@@ -6,6 +11,7 @@ function myFunction(elementId) {
         x.style.display = "none";
     }
 }
+// End Owner: Chloe Nevin
 
 ////////////////////
 // Owner : Hannah Rivet
@@ -15,4 +21,19 @@ function myFunction(elementId) {
 function contactFunction() {
     alert("Thank you for submitting."+"\n"+"One of our associates will be in contact with you very soon. Have a good day!");
 }
-// End Owner: Shawn Morgan
+// End Owner: Hannah Rivet 
+
+
+////////////////////
+// Owner : Aysha Qureshi
+// Date  : Dec 7
+// From  : https://github.com/sponsors/miguelznunez
+////////////////////
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+// End Owner: Aysha Qureshi
